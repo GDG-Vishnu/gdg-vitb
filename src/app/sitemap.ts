0be4648e30057a20e764430283ac1077f2026afc
GurunadaRao <gurunadharao5718@gmail.com>
2026-01-09 17:44:51 +0000

    resolved the console log errors

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = "https://gdgvitb.in";
-  const currentDate = new Date();
+  const currentDate = new Date().toISOString();
   
   return [
     {
